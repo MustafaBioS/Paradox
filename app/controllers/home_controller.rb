146d276d38c883484ac01c38f81297941546ee9c
MustafaBioS <mustaafahany@gmail.com>
2026-04-10 12:07:45 +0200

    ADD - Home Page Complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,22 @@
 class HomeController < ApplicationController
   before_action :require_login
-
   def index
+    @messages = [
+      "Bomboclat Message #1",
+      "Bomboclat Message #2",
+      "Bomboclat Message #3",
+      "Bomboclat Message #4",
+      "Bomboclat Message #5",
+      "Bomboclat Message #6",
+      "Bomboclat Message #7",
+      "Bomboclat Message #8",
+      "Bomboclat Message #9",
+      "Bomboclat Message #10",
+      "Bomboclat Message #11",
+      "Bomboclat Message #12",
+      "Bomboclat Message #13",
+      "Bomboclat Message #14",
+      "Bomboclat Message #15",
+    ]
   end
 end
