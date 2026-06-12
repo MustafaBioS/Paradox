02735d213b9ca8671e22319ba2a3780f7d42bcfa
MustafaBioS <mustaafahany@gmail.com>
2026-06-12 16:46:34 +0300

    fix

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :require_login
 
-  params.require(:shop_item).permit(:name, :hours, :description, :image)
+  def item_params
+    params.require(:shop_item).permit(:name, :hours, :description, :image)
+  end
 end
